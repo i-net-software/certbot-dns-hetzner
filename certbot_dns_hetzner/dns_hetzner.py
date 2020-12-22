b8c176fc60d03abf46847ec603317346d87c7030
Gerry Weißbach <gerry.w@gammaproduction.de>
2020-12-22 12:37:25 +0100

    Keep spaces.

--- certbot_dns_hetzner/dns_hetzner.py
+++ certbot_dns_hetzner/dns_hetzner.py
@@ -63,7 +63,7 @@ class Authenticator(dns_common.DNSAuthenticator):
                     record_id,
                     "TXT",
                     formated_name,
-                    '"' + '\\n'.join([record_value, validation]) + '"',
+                    " ".join([record_value, validation]),
                     TTL
                 )
 
@@ -96,7 +96,7 @@ class Authenticator(dns_common.DNSAuthenticator):
 
             try:
                 record_id = client.get_record_id_by_name(zone_id, formated_name)
-                record_values = client.get_record_value_by_name(zone_id, formated_name).split('\\n')
+                record_values = client.get_record_value_by_name(zone_id, formated_name).split(" ")
             except (_RecordNotFoundException) as exception:
                 # this one may be ok ... we do want it removed
                 return
@@ -112,7 +112,7 @@ class Authenticator(dns_common.DNSAuthenticator):
                     record_id,
                     "TXT",
                     formated_name,
-                    '"' + '\\n'.join(record_values) + '"',
+                    " ".join(record_values),
                     TTL
                 )
 
